ce9d2b4f0a69833db0d0d20f227f3288855ea395
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-13 15:59:57 +0900

    バリデーションの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 2)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {password = '1a' + Faker::Internet.password(min_length: 6)}
     birthdate             {Faker::Date.backward }
     password_confirmation {password}
     last_name {Gimei.last.kanji }
