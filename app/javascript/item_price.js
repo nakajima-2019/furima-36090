48cde0bb2cf482053082dbd37b657d1223355680
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-15 17:38:06 +0900

    エラーハンドリングの実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,6 +5,6 @@ window.addEventListener('load', () => {
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1)
     const profit = document.getElementById("profit");
-    profit.innerHTML = Math.floor(inputValue - addTaxDom)
+    profit.innerHTML = Math.floor (inputValue) - (addTaxDom.innerHTML)
 })
 });
--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
   validates :image, presence: true
-  
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :item_state_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :delivery_burden_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "can't be blank or out of range or 販売価格は半角で入力してください."}, format: { with: /\A[0-9]+\z/ }
 
 end
