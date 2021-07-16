413bb5eee9ded99c82e3ef8b4e246fee1391ab87
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-16 14:35:01 +0900

    単体テストコードの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,14 +9,24 @@ class Item < ApplicationRecord
   belongs_to :delivery_area
   belongs_to :delivery_day
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :image, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :item_state_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_burden_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_area_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "can't be blank or out of range or 販売価格は半角で入力してください."}, format: { with: /\A[0-9]+\z/ }
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+  end
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"}  do
+    validates :category_id
+    validates :item_state_id
+    validates :delivery_burden_id
+    validates :delivery_area_id
+    validates :delivery_day_id
+  end
+
+  validates :selling_price,presence: true
+  validates_inclusion_of :selling_price,in:300..9999999, message: "out of range"
+  validates :selling_price,format: { with: /\A[0-9]+\z/, message: "half-width input"}
+
+  # validates_inclusion_of :selling_price,in: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "out of range"}
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe "出品登録" do
+    context '新規出品できないとき' do
+      it 'nameが空では登録できない' do
+        @item.name = '' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+      it 'descriptionが空では登録できない' do
+        @item.description = '' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Description can't be blank"
+      end
+      it 'imageが空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+      it 'categoryが空では登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+      it 'item_stateが空では登録できない' do
+        @item.item_state_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Item state can't be blank"
+      end
+      it 'delivery_burdenが空では登録できない' do
+        @item.delivery_burden_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery burden can't be blank"
+      end
+      it 'delivery_areaが空では登録できない' do
+        @item.delivery_area_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery area can't be blank"
+      end
+      it 'delivery_dayが空では登録できない' do
+        @item.delivery_day_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery day can't be blank"
+      end
+      it 'selling_priceが空では登録できない' do
+        @item.selling_price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Selling price can't be blank"
+      end
+      it 'selling_priceが300未満では登録できない' do
+        @item.selling_price = '299' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Selling price out of range"
+      end
+      it 'selling_priceが9,999,999より大きいと登録できない' do
+        @item.selling_price = '10000000' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Selling price out of range"
+      end
+      it 'selling_priceが半角数字でないと登録できない' do
+        @item.selling_price = '１０００' 
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Selling price out of range"
+      end
+    end
+  end
 end
