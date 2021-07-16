9fbf272df86bb3ebc5db282cdb9c27c5960d629b
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-16 17:29:57 +0900

    指摘事項の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, only:[:new, :create]
+
   def index
   end
 
   def new
     @item = Item.new
   end
-
 
   def create
     @item = Item.new(item_params)
