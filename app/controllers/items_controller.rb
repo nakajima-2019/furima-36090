7e57b54489461cd52a207966c078f155f3a10e2b
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-19 17:58:59 +0900

    商品編集ページへの遷移実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :image, :description, :category_id, :item_state_id, :delivery_burden_id, :delivery_area_id, :delivery_day_id, :selling_price).merge(user_id: current_user.id)
