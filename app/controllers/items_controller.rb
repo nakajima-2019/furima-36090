3e2ea9360295a76442503e2704e2d01679807633
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-20 11:40:04 +0900

    ログイン状態の有無でルート条件を定義した

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.all.order(id: 'DESC')
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
   end
 
   def update
