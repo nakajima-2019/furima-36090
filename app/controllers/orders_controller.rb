d310c100efe3c6f44785fe1a86c460af1d3d5ae4
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-21 12:45:03 +0900

    購入画面に遷移できる

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,19 @@
+class OrdersController < ApplicationController
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:price)
+  end
+
+end
