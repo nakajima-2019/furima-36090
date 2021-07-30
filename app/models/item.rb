2771cf6a15d705fb1798effb670c66c37d4113f8
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-30 14:32:26 +0900

    アソシエーションを変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_many_attached :images
   has_one :order
 
   extend ActiveHash::Associations::ActiveRecordExtensions
