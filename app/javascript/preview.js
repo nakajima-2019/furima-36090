35bdb68ae7fa550f40586d7a3bfdc169d36b846f
nakajima-2019 <52471127+nakajima-2019@users.noreply.github.com>
2021-07-30 15:38:24 +0900

    複数枚投稿の完了

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-
+  console.log("ok")
   if ( document.getElementById('item-image')){
     const ImageList = document.getElementById('image-list');
 
@@ -7,22 +7,35 @@ document.addEventListener('DOMContentLoaded', function(){
     const createImageHTML = (blob) => {
        // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div');
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
       // 表示する画像を生成
       const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
       blobImage.setAttribute('class', "item-img");
+
+      // ファイル選択ボタンを生成
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `message_image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'message[images][]')
+      inputHTML.setAttribute('type', 'file')
+
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
+      imageElement.appendChild(inputHTML)
       ImageList.appendChild(imageElement);
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
     };
 
     document.getElementById('item-image').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+ 
       const file = e.target.files[0];
       const blob = window.URL.createObjectURL(file);
 
